f1e7b959a6e808ba2c75ea8c2ecab7a179ac602b
Nicolas Da Mutten <nicolas.damutten@contactify.ch>
2025-02-21 14:31:32 +0100

    Revert "WIP: quick test to see if separating works as a workaround"
    This reverts commit c9f2a43154c3a949d8a7b0a0f60d79e507203df4.

--- data.tf
+++
@@ -1,5 +0,0 @@
-data "azurerm_key_vault_secret" "cert_secret" {
-  count        = 1
-  name         = "test"
-  key_vault_id = provider::azurerm::normalise_resource_id("abc")
-}
--- main.tf
+++ main.tf
@@ -6,3 +6,9 @@ terraform {
     }
   }
 }
+
+data "azurerm_key_vault_secret" "cert_secret" {
+  count        = 1
+  name         = "test"
+  key_vault_id = provider::azurerm::normalise_resource_id("abc")
+}
