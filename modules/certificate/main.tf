23e8419baab9832e0c6433dc83c53bb627548627
Nicolas Da Mutten <nicolas.damutten@contactify.ch>
2025-02-21 14:15:13 +0100

    fix: this seems to trigger the bug

--- modules/certificate/main.tf
+++ modules/certificate/main.tf
@@ -7,10 +7,9 @@ terraform {
   }
 }
 
-resource "azurerm_app_service_certificate_order" "cert_order" {
-  name                = "abc"
-  resource_group_name = var.resource_group.name
-  location            = "global"
-  distinguished_name  = "CN=def"
-  product_type        = "WildCard"
+data "azurerm_key_vault_secret" "cert_secret" {
+  count = length(azurerm_app_service_certificate_order.cert_order.certificates) > 0 ? 1 : 0
+  name  = azurerm_app_service_certificate_order.cert_order.certificates[0].key_vault_secret_name
+  # There seems to be a bug in Azure, so we correct the key_vault_id here
+  key_vault_id = provider::azurerm::normalise_resource_id(azurerm_app_service_certificate_order.cert_order.certificates[0].key_vault_id)
 }
