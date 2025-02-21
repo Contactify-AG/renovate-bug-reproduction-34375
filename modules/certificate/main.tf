3b5d5b4b44b87ea392f6e8d8be2779961dcd44e9
Nicolas Da Mutten <nicolas.damutten@contactify.ch>
2025-02-21 14:14:09 +0100

    fix: narrow down reproduction more

--- modules/certificate/main.tf
+++ modules/certificate/main.tf
@@ -6,3 +6,11 @@ terraform {
     }
   }
 }
+
+resource "azurerm_app_service_certificate_order" "cert_order" {
+  name                = "abc"
+  resource_group_name = var.resource_group.name
+  location            = "global"
+  distinguished_name  = "CN=def"
+  product_type        = "WildCard"
+}
