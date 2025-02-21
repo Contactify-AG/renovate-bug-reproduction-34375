7284535fed3efb8a74a99629ede32b98c596982d
Nicolas Da Mutten <nicolas.damutten@contactify.ch>
2025-02-21 14:25:43 +0100

    fic: further simplify reproduciton

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ terraform {
 }
 
 data "azurerm_key_vault_secret" "cert_secret" {
-  count = length(azurerm_app_service_certificate_order.cert_order.certificates) > 0 ? 1 : 0
-  name  = azurerm_app_service_certificate_order.cert_order.certificates[0].key_vault_secret_name
-  # There seems to be a bug in Azure, so we correct the key_vault_id here
-  key_vault_id = provider::azurerm::normalise_resource_id(azurerm_app_service_certificate_order.cert_order.certificates[0].key_vault_id)
+  count        = 1
+  name         = "test"
+  key_vault_id = provider::azurerm::normalise_resource_id("abc")
 }
